209d4cd8c65ec639688a6578bf7057f03d7abd90
Vivek20500 <141113863+Vivek20500@users.noreply.github.com>
2026-03-24 17:27:10 +0530

    problem Page ui updated

--- src/components/problemStatement/domainBlock.jsx
+++ src/components/problemStatement/domainBlock.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 
+function Arrow(){
+  return (
+    <svg width="30" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+  <g stroke="oklch(57.7% 0.245 27.325)" 
+     stroke-width="4" 
+     stroke-linecap="butt"
+     stroke-linejoin="miter"
+     fill="none">
+    
+    <line x1="8" y1="20" x2="26" y2="20" />
+    
+    <line x1="20" y1="12" x2="30" y2="21.5" />
+    <line x1="20" y1="28" x2="30" y2="19.5" />
+    
+  </g>
+</svg>
+  )
+}
+
 function DomainBlock(props) {
   return (
     <div className="w-full h-full flex items-center justify-center">
       
-      <div className="
+      {/* <div className="
         w-full max-w-[18rem] h-full min-h-[220px] max-h-[220px] p-3 sm:p-4 rounded-xl border-2 border-[#621b00]
         bg-[#a9a298] text-white text-center
         shadow-[0px_0px_0px_#621b00]
@@ -39,7 +58,20 @@ function DomainBlock(props) {
             View Details
         </button>
 
-      </div>
+      </div> */}
+      <div class="border-r border-b border-outline-variant/20 p-10 hover:bg-[#f8c986] transition-all duration-300 group hover:shadow-[0px_0px_10px_#000000ff] ">
+<div class="flex justify-between items-start mb-12">
+<span class="font-headline text-[11px] font-black text-red-600 tracking-widest text-secondary">{props.title}</span>
+<span class="material-symbols-outlined text-primary opacity-20 group-hover:opacity-100 transition-opacity">{props.icon}</span>
+</div>
+<h3 class="font-headline text-2xl font-bold mb-4 text-primary">{props.domain}</h3>
+<p class="font-body text-on-surface-variant mb-10 leading-relaxed min-h-[80px]">
+                        {props.description}
+                    </p>
+<button class="font-headline text-[10px] font-bold text-red-600 uppercase tracking-[0.2em] text-secondary flex items-center gap-3 group-hover:gap-5 transition-all" onClick={() => window.open(props.link, '_blank')}>
+                        VIEW DETAILS <Arrow />
+</button>
+</div>
 
     </div>
   )
