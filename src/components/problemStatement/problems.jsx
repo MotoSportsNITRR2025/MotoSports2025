aad0d4fed7add9a90618580454ef2174789459f7
Vivek20500 <141113863+Vivek20500@users.noreply.github.com>
2026-03-25 14:24:04 +0530

    form links updated

--- src/components/problemStatement/problems.jsx
+++ src/components/problemStatement/problems.jsx
@@ -9,12 +9,12 @@ const descriptions=[
   "Create responsive steering systems that translate driver intent into perfect control on the track.",
   "Design systems that absorb shocks, maximize grip, and keep the vehicle stable on any terrain."
 ]
-const links=["https://drive.google.com/file/d/1UtNuzAtDF_XGAKmX3yHNjAdg2x2BKpqM/view?usp=sharing",
-    "https://drive.google.com/file/d/1B0OkZ0Fdtf-vpZ0K7k48EZhKvT2xipZC/view?usp=sharing",
-    "https://drive.google.com/file/d/1z-bS3YmlWSnwTYZX0vvVqzbapQs_gH5w/view?usp=drive_link",
-    "https://drive.google.com/file/d/1avy1Ovyv1IbFKcpm2kXwbLrNrsuWzst5/view?usp=sharing",
-    "https://drive.google.com/file/d/1syxyCt-a_s8aB-rDSDbGCxnaVI1sh74-/view?usp=sharing",
-    "https://drive.google.com/file/d/1aXLQglTSIbkSxBZz8_AZfqms3-lnPhQy/view?usp=sharing"
+const links=["https://drive.google.com/file/d/16zt23g8Z1LUAlgchDS965guUiQrikQ3a/view?usp=sharing",
+    "https://drive.google.com/file/d/1smSmnODY5WjP7Gk6vNOIC2CxRiQjRJyJ/view?usp=sharing",
+    "https://drive.google.com/file/d/187Iu4sQfYhsUozjRxUJSjTMEk9mV7oMF/view?usp=sharing",
+    "https://drive.google.com/file/d/16FlPZ1fOWjfLym96kLRtsiqtisHniH-H/view?usp=sharing",
+    "https://drive.google.com/file/d/1NuiLyB_qX9GmPLMQU-z1NTOc0_ZhODxn/view?usp=sharing",
+    "https://drive.google.com/file/d/1Z_40RwqsJXmU5rukktf6uylDQWKSMuRx/view?usp=sharing"
 ]
 
 const icons=["speed","bolt","settings","build","control_camera","tune"]
@@ -35,31 +35,56 @@ function ProblemStatements() {
             <DomainBlock domain="Steering" description={descriptions[4]} link={links[4]} icon={icons[4]} title={titles[4]} />
             <DomainBlock domain="Suspension" description={descriptions[5]} link={links[5]} icon={icons[5]} title={titles[5]} />
         </div>
-        <div className="w-full max-w-7xl mx-auto my-6 text-center">
-  
-            <div className="h-[2px] bg-gradient-to-r from-transparent via-[#621b00] to-transparent"></div>
-            
-            <div className="group relative px-8 py-4 cursor-pointer">
-        
-
-        {/* Text */}
-        <p className="py-4 text-lg font-semibold tracking-wide text-[#621b00] 
-                      transition-all duration-300 
-                      group-hover:tracking-widest group-hover:text-[#c75000]"
-                      onClick={() => window.open('https://forms.gle/9sz7pUGvK7A1NLiN6', '_blank')}>
-          Submit Here →
-        </p>
-
-        {/* Background glow on hover */}
-        <div className="absolute inset-0 -z-10 opacity-0 
-                        bg-[#fff1de] blur-md 
+        {/* <div className="w-full max-w-7xl mx-auto my-6 text-center">
+          <div className="h-[2px] bg-gradient-to-r from-transparent via-[#621b00] to-transparent"></div>
+          <div className="group relative px-8 py-4 cursor-pointer">
+          <p className="py-4 text-lg font-semibold tracking-wide text-[#621b00] 
                         transition-all duration-300 
-                        group-hover:opacity-100"></div>
-
-        </div>
-            
-            <div className="h-[2px] bg-gradient-to-r from-transparent via-[#621b00] to-transparent"></div>
-
+                        group-hover:tracking-widest group-hover:text-[#c75000]"
+                        onClick={() => window.open('https://forms.gle/9sz7pUGvK7A1NLiN6', '_blank')}>
+            Submit Here →
+          </p>
+          <div className="absolute inset-0 -z-10 opacity-0 
+                          bg-[#fff1de] blur-md 
+                          transition-all duration-300 
+                          group-hover:opacity-100"></div>
+          </div>
+          <div className="h-[2px] bg-gradient-to-r from-transparent via-[#621b00] to-transparent"></div>
+        </div> */}
+        <div
+          class="mt-10 sm:mt-14 lg:mt-20 w-full max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between gap-6 sm:gap-8 lg:gap-12 p-6 sm:p-8 lg:p-12 bg-on-surface text-surface relative overflow-hidden bg-[#181919] text-[#faf9f8]"
+        >
+          <div class="absolute inset-0 opacity-10 mix-blend-overlay">
+            <img
+              class="w-full h-full object-cover"
+              data-alt="Abstract macro shot of carbon fiber texture with dramatic studio lighting and sharp diagonal lines"
+              src="https://lh3.googleusercontent.com/aida-public/AB6AXuBtfo6Xmdgi2RkwLQKDMMxb4hRaUOjx4pDhlpPwqhMLebGkn6sjWgeFBmZ6aBtBz_7ZhsRTo6p4w4FYdWFjrkZoc9N2DExFqfIdxInEWCz7gujNVI2jj9fscOmqD6ZEguijBf4TQGQAG_fBuczTzf9ifptVUlWbpSRx1NXWYJwXAoRcJDKtmhh_RJnkq221fi2zBB3f2HZwh8nZ4DhmedLGVLuEZz_2f9XR-PdAD3CskYxKBM6IF2mw-QixEh-NsxbnYcL2a1ZGdA"
+            />
+          </div>
+          <div class="relative z-10 max-w-xl ">
+            <h2
+              class="text-4xl font-headline font-black uppercase tracking-tighter mb-4 italic"
+            >
+              Ready to fuel the mission?
+            </h2>
+            <p class="text-surface-container-high opacity-80 font-body">
+              Join the elite creative operations squad. Your strategic input is
+              the aerodynamic edge we need on the track.
+            </p>
+          </div>
+          <div class="w-full md:w-auto flex flex-col gap-3 sm:gap-4 md:gap-5">
+            <button
+            class="relative z-10 custom-gradient-btn w-full sm:w-auto px-6 sm:px-8 lg:px-10 py-4 sm:py-5 lg:py-6 text-on-primary font-headline font-black uppercase tracking-tighter text-base sm:text-lg lg:text-xl scale-100 hover:scale-105 transition-all active:scale-95 whitespace-nowrap bg-[#9e491b]"
+          onClick={() => window.open('https://forms.gle/EpXeeJGkKsK7oZWdA', '_blank')}>
+            Submit Tech Entry →
+          </button>
+          <button
+            class="relative z-10 custom-gradient-btn w-full sm:w-auto px-6 sm:px-8 lg:px-10 py-4 sm:py-5 lg:py-6 text-on-primary font-headline font-black uppercase tracking-tighter text-base sm:text-lg lg:text-xl scale-100 hover:scale-105 transition-all active:scale-95 whitespace-nowrap bg-[#9e491b]"
+          onClick={() => window.open('https://forms.gle/5ZyACpYkMNYR6CGq9', '_blank')}>
+            Submit Non-Tech Entry →
+          </button>
+          </div>
+          
         </div>
         
     </main>
